e0df9661c0d8ba5bd251f0a1a83863197bf2fc17
Alexis Carter <alexis-developer@hotmail.com>
2021-02-26 09:53:25 +1100

    First commit - HTML, SCSS, JavaScript

---
+++ README.md
@@ -0,0 +1,19 @@
+### Morse Code Translator
+
+## HTML, SCSS & JavaScript
+
+#Specifications:
+
+- User should be able to input morse code string
+    - Get english back (after hitting a submit button)
+
+- User should be able to input english string
+    - User should be able to input english string
+
+#Still to add:
+
+- Clear button
+- Restyle output section
+- Add text "typing" animation for result output text
+- Rework CSS format using BEM
+- JavaScript - testing and add more validation checks
---
+++ main.js
@@ -0,0 +1,117 @@
+const alphabet = {  
+    "0":"-----",
+    "1":".----",
+    "2":"..---",
+    "3":"...--",
+    "4":"....-",
+    "5":".....",
+    "6":"-....",
+    "7":"--...",
+    "8":"---..",
+    "9":"----.",
+    "a":".-",
+    "b":"-...",
+    "c":"-.-.",
+    "d":"-..",
+    "e":".",
+    "f":"..-.",
+    "g":"--.",
+    "h":"....",
+    "i":"..",
+    "j":".---",
+    "k":"-.-",
+    "l":".-..",
+    "m":"--",
+    "n":"-.",
+    "o":"---",
+    "p":".--.",
+    "q":"--.-",
+    "r":".-.",
+    "s":"...",
+    "t":"-",
+    "u":"..-",
+    "v":"...-",
+    "w":".--",
+    "x":"-..-",
+    "y":"-.--",
+    "z":"--..",
+    " ":"/",
+    "!":"-·-·--",
+    ".":"·-·-·-",
+    ",":"--··--"
+};
+
+const morse = {
+    "-----": "0",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+    "/": " ",
+    "-·-·--": "!",
+    "·-·-·-": ".",
+    "--··--": ",",
+};
+
+const hideTitlePage = () => {
+
+  const titlePage = document.getElementById("titlePage");
+  if (titlePage.style.display === "none") {
+    titlePage.style.display = "block";
+  } else {
+    titlePage.style.display = "none";
+  }
+}
+
+const alphabetToMorse = () => {
+
+  const userInput = document.getElementById("alphabetInput").value; 
+	const lowerCase = userInput.toLowerCase().split(''); 
+	const mappedLetters = lowerCase.map((morse) => {  
+	return alphabet[morse];
+})
+	const result = mappedLetters.join(' '); 
+	document.getElementById("morseOutput").innerHTML = result; 
+}
+
+const morseToAlphabet = () => {
+
+  const userInput = document.getElementById("morseInput").value;  
+	const splitMorse = userInput.split(' '); 
+	const mappedMorse = splitMorse.map((alphabet) => { 
+	return morse[alphabet];
+})
+	const result = mappedMorse.join(''); 
+	document.getElementById("alphabetOutput").innerHTML = result;
+}
